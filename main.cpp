9ebeb32737a6fd206ceb6ff7b92811130dfa32d0
6eorgee <ivgn00@gmail.com>
2020-10-28 16:25:57 +0300

    almost generating file

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <boost/program_options.hpp>
+
+#define MAX_PACKET_SIZE 100
 
 using namespace std;
 namespace po = boost::program_options;
 
 
+int random_number(int max = 10 * MAX_PACKET_SIZE)
+{
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<int> uniform_dist(1, max);
+    return uniform_dist(e1);
+}
+
 void generate_file()
 {
 	string filename = "generated.txt";
 	std::ofstream ostrm(filename, std::ios::binary);
-	ostrm << 123 << "abc" << '\n';      
+
+	int str_count = 100 + random_number(50);
+	for (int i = 0; i < str_count; i++)
+	{
+		int str_len = random_number();
+		for (int j = 0; j < str_len; j++)
+		{
+			ostrm << j % 10;
+		}
+		ostrm << '\n';
+	}
 }
-
-
 
 int main(int ac, char* av[])
 {
